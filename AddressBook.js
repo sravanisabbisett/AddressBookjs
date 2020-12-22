f19ccb2a8cc67f684d37c0391d5346dc1aa94469
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-22 17:41:51 +0530

    ADDED:constructors to create contacts in addressbook

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,12 @@
-console.log("AddressBook");
+class Contact{
+    constructor(...params){
+        this.firstname=params[0];
+        this.lastname=params[1];
+        this.address=params[2];
+        this.city=params[3];
+        this.state=params[4];
+        this.zip=params[5];
+        this.phonenumber=params[6];
+        this.email=params[7];
+    }
+}
